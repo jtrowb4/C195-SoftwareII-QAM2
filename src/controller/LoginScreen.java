30494aab65326347847e9323605add840558860f
jtrowb4 <jtrowb4@wgu.edu>
2022-03-12 16:36:46 -0600

    javafx library adjusted. Application loads LoginScreen and LoginScreen.fxml

--- src/controller/LoginScreen.java
+++ src/controller/LoginScreen.java
@@ -1,4 +1,26 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class LoginScreen {
+
+    public Button loginButton;
+    public Button exitButton;
+
+    public void onButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onExit(ActionEvent actionEvent) {
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,5 @@
 package main;
+
 /**
  * class Main.java
  */

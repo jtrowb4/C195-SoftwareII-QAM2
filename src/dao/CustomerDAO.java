1558d46c369a888566da46bb506c60d88526f95f
jtrowb4 <jtrowb4@wgu.edu>
2022-03-17 21:12:46 -0500

    minor changes

--- src/dao/CustomerDAO.java
+++ src/dao/CustomerDAO.java
@@ -3,7 +3,6 @@ package dao;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.Customer;
-import model.User;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
